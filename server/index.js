2357eaa8582cfda8b32d3939a882c52fa4d15f67
Arnav Singh <arnav.kumarr2016@gmail.com>
2026-02-22 23:16:18 +0530

    Fix CORS origin URL by removing trailing slash

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:["http://localhost:4000", "https://study-notion-arnab.netlify.app/"],
+        origin:["http://localhost:4000", "https://study-notion-arnab.netlify.app"],
         credentials:true
     })
 )
